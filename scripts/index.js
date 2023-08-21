7cac4310949194a0ff89f1e648171d8700b96db2
Albert Martinez <ialbertmartinez@outlook.com>
2023-08-21 03:00:46 -0700

    feature: able to delete cards & open pictures in modal overlay

--- scripts/index.js
+++ scripts/index.js
@@ -26,81 +26,77 @@ const initialCards = [
   },
 ];
 
+// wrappers
 const page = document.querySelector(".page");
 const pageWrapper = page.querySelector(".page__wrapper");
-
+const modalEdit = page.querySelector("#modal-edit");
+const modalAdd = page.querySelector("#modal-add");
+const modalImg = page.querySelector("#modal-image");
 const profile = pageWrapper.querySelector(".profile");
+
+// buttons & other dom elements
+const cards = pageWrapper.querySelector(".cards");
+const cardsGallery = pageWrapper.querySelector(".cards__gallery");
+const cardTemplate = pageWrapper.querySelector("#card").content;
+
 const profileTitle = profile.querySelector(".profile__title");
 const profileSubtitle = profile.querySelector(".profile__subtitle");
+const editBtn = pageWrapper.querySelector(".profile__edit-button");
+const addBtn = pageWrapper.querySelector(".profile__add-button");
+const editCloseBtn = modalEdit.querySelector(".modal__close-button");
+const addCloseBtn = modalAdd.querySelector(".modal__close-button");
+const likeBtn = cards.querySelectorAll(".card__like-button");
+const modalImage = modalImg.querySelector(".modal__image");
+const modalCaption = modalImg.querySelector(".modal__caption");
 
-// edit form variables
-const modalEdit = page.querySelector("#modal-edit");
+// form & form data
 const modalEditForm = modalEdit.querySelector(".modal__form");
-
+const modalAddForm = modalAdd.querySelector(".modal__form");
 const inputTitle = modalEdit.querySelector(".modal__input_type_title");
 const inputSubtitle = modalEdit.querySelector(".modal__input_type_subtitle");
-
-const editBtn = pageWrapper.querySelector(".profile__edit-button");
-const editCloseBtn = modalEdit.querySelector(".modal__close-button");
-
-// add form variables
-const modalAdd = page.querySelector("#modal-add");
-const modalAddForm = modalAdd.querySelector(".modal__form");
-
 const inputCardTitle = modalAdd.querySelector(".modal__input_type_title");
 const inputCardUrl = modalAdd.querySelector(".modal__input_type_subtitle");
 
-const addBtn = pageWrapper.querySelector(".profile__add-button");
-const addCloseBtn = modalAdd.querySelector(".modal__close-button");
-
-const cards = pageWrapper.querySelector(".cards");
-const likeBtn = cards.querySelectorAll(".card__like-button");
-// when clicking on a card image
-// a modal window with the image @ full width and height will be shown
-// the image width should not exceed 75vw
-// the image height should not exceed 75vh
-// vertical padding or margin (both top and botton) should be 90px each
-// image should be centered horizontally and vertically
-
-function openModalForm(modal) {
+function openModal(modal) {
   modal.classList.add("modal_opened");
   inputTitle.value = profileTitle.innerText;
   inputSubtitle.value = profileSubtitle.innerText;
 }
 
-function closeModalForm(modal) {
+function closeModal(modal) {
   modal.classList.remove("modal_opened");
+  inputCardUrl.value = "";
+  inputCardTitle.value = "";
 }
 
-function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
+function handleProfileFormSubmit(e) {
+  e.preventDefault();
   profileTitle.innerText = inputTitle.value;
   profileSubtitle.innerText = inputSubtitle.value;
-  closeModalForm(modalEdit);
+  closeModal(modalEdit);
 }
 
-function handleAddFormSubmit(evt) {
-  evt.preventDefault();
+function handleAddFormSubmit(e) {
+  e.preventDefault();
   const cardData = {
     link: inputCardUrl.value,
     name: inputCardTitle.value,
   };
   const cardsGallery = pageWrapper.querySelector(".cards__gallery");
   cardsGallery.prepend(renderCard(cardData));
+  closeModal(modalAdd);
   inputCardUrl.value = "";
   inputCardTitle.value = "";
-  closeModalForm(modalAdd);
 }
 
 function likeIt(e) {
   const classes = e.target.classList;
-  if (classes.contains("card__like-button") && classes.contains("liked")) {
-    classes.remove("liked");
-  } else if (
-    classes.contains("card__like-button") &&
-    !classes.contains("liked")
-  ) {
-    classes.add("liked");
+  if (classes.contains("card__like-button")) {
+    if (classes.contains("liked")) {
+      classes.remove("liked");
+    } else {
+      classes.add("liked");
+    }
   }
 }
 
@@ -109,29 +105,41 @@ function deleteIt(e) {
 }
 
 function renderCard(data) {
-  const cardTemplate = pageWrapper.querySelector("#card").content;
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
   const deleteBtn = cardElement.querySelector(".card__delete-button");
+  const modalCloseBtn = modalImg.querySelector(".modal__close-button");
   cardImage.src = data.link;
   cardImage.alt = data.name;
   cardTitle.textContent = data.name;
+
   cardElement.addEventListener("click", likeIt);
   deleteBtn.addEventListener("click", deleteIt);
+  modalCloseBtn.addEventListener("click", function () {
+    closeModal(modalImg);
+  });
+
+  // img modal
+  cardImage.addEventListener("click", () => {
+    modalImage.src = data.link;
+    modalImage.alt = data.alt;
+    modalCaption.textContent = data.name;
+    openModal(modalImg);
+  });
+
   return cardElement;
 }
 
-editBtn.addEventListener("click", () => openModalForm(modalEdit));
-editCloseBtn.addEventListener("click", () => closeModalForm(modalEdit));
-modalEditForm.addEventListener("submit", () => handleProfileFormSubmit(event));
+editBtn.addEventListener("click", () => openModal(modalEdit));
+editCloseBtn.addEventListener("click", () => closeModal(modalEdit));
+modalEditForm.addEventListener("submit", handleProfileFormSubmit);
 
-addBtn.addEventListener("click", () => openModalForm(modalAdd));
-addCloseBtn.addEventListener("click", () => closeModalForm(modalAdd));
+addBtn.addEventListener("click", () => openModal(modalAdd));
+addCloseBtn.addEventListener("click", () => closeModal(modalAdd));
 modalAddForm.addEventListener("submit", handleAddFormSubmit);
 
 initialCards.forEach(function (card) {
-  const cardsGallery = pageWrapper.querySelector(".cards__gallery");
   const cardElement = renderCard(card);
   cardsGallery.prepend(cardElement);
 });
